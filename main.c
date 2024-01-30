e493969ef1d5b982771b97c6a910c5dbe6d66eca
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-01-30 01:48:21 +0100

    last additions

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: mevangel <mevangel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 20:59:22 by mevangel          #+#    #+#             */
-/*   Updated: 2024/01/28 19:13:35 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/01/30 00:47:53 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-/* Checks for: 
+/* 
 	- Accepts only numeric characters (0123456789) in the arguments, '+' or '-' not accepted
 	- rejects empty arguments
 	- rejects 0 as any argument
@@ -41,38 +41,28 @@ bool	ft_input_is_valid(int argc, char **argv)
 	return (true);
 }
 
-void	ft_init_sdata(int argc, char **argv, t_data *data)
+bool ft_init_sdata(int argc, char **argv, t_data *data)
 {
-	data->num_philos = ft_atoui(argv[0]);
-	data->time_to_die = ft_atoui(argv[1]);
-	data->time_eating = ft_atoui(argv[2]);
-	data->time_sleeping = ft_atoui(argv[3]);
-	data->max_meals = 0;
-	if (argc == 6)
-		data->max_meals = ft_atoui(argv[4]);
+	data->num_philos = (int)ft_atoui(argv[0]);
+	if (data->num_philos > 200)
+		return (printf(ERROR "philos can not be more than 200.\n"), false);
+	data->t_die = ft_atoui(argv[1]);
+	data->t_eat = ft_atoui(argv[2]);
+	data->t_sleep = ft_atoui(argv[3]);
+	data->notepme = 0;
 	data->any_dead = false;
-	// Should I also save the multiplication max_meals * num_of_philos ?
-
+	data->philos_done = 0;
+	if (argc == 6)
+		data->notepme = ft_atoui(argv[4]);
+	if (pthread_mutex_init(&data->dead, NULL) || pthread_mutex_init(&data->done,
+		NULL) || pthread_mutex_init(&data->print, NULL)) //returns 0 in success
+			return (printf(ERROR "pthread_mutex_init_failed.\n"), false);
+	return (true);
 }
-
-// bool ft_init_sdata(int argc, char **argv, t_data *data)
-// {
-// 	data->num_philos = ft_atoui(argv[0]);
-// 	data->time_to_die = ft_atoui(argv[1]);
-// 	data->time_eating = ft_atoui(argv[2]);
-// 	data->time_sleeping = ft_atoui(argv[3]);
-// 	data->max_meals = 0;
-// 	if (argc == 6)
-// 		data->max_meals = ft_atoui(argv[4]);
-// 	data->any_dead = false;
-// 	// Should I also save the multiplication max_meals * num_of_philos ?
-
-// 	return (true);
-// }
 
 bool	ft_init_sphilo(t_data *data)
 {
-	unsigned int	id;
+	int	id;
 
 	id = 0;
 	data->philo = (t_philo *)malloc(data->num_philos * sizeof(t_philo));
@@ -82,11 +72,11 @@ bool	ft_init_sphilo(t_data *data)
 	{
 		data->philo[id - 1].id = id;
 		// data->philo[id - 1].thread = 0; //do I actually need that?
-		data->philo[id - 1].started_eat = current_mtime();
+		data->philo[id - 1].eat_start = current_mtime();
 		data->philo[id - 1].times_ate = 0;
-		data->philo[id - 1].is_eating = false;
+		data->philo[id - 1].finished = false;
 		if (pthread_mutex_init(&data->philo[id - 1].r_fork, NULL)) //returns 0 in success
-			return (ft_error("mutex_init failed", data->philo, --id, false), false);
+			return (ft_exit("mutex_init failed", data->philo, --id, false), false);
 		if (id > 1)
 			data->philo[id - 1].l_fork = &data->philo[id - 2].r_fork;
 	}
@@ -107,70 +97,39 @@ void	*test_routine(void *arg)
 
 void	*ft_death_patrol(void *arg)
 {
-	
+	(void)arg;
+	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	t_data			data;
-	unsigned int	i;
-	pthread_t		death;
+	t_data		data;
+	int			i;
+	pthread_t	check_dead;
 	// t_philo	*philos_ptr;
 	
 	if (!ft_input_is_valid(argc, ++argv))
 		return(printf(ERROR "invalid input\n"), 0); //or i could change it directly to printf
-	ft_init_sdata(argc, argv, &data);
-	// if (!ft_init_sdata(argc, argv, &data))
-	// 	return(printf(ERROR "malloc failed\n"), 1); //i don't need to free the data->philo cause if the malloc failed the pointer is null
+	if (!ft_init_sdata(argc, argv, &data))
+		return(printf("Failed to initialize data.\n"), 1); //i don't need to free the data->philo cause if the malloc failed the pointer is null
 	if (!ft_init_sphilo(&data)) // it returns non zero only if it failed
 		return(printf(ERROR "failed to initialize philos\n"), 2);
 	data.start_time = current_mtime();
 	i = 0;
+	//? i create the philo threads first:
 	while (i++ < data.num_philos) //the comparison is with i as 0 but i inside the loop starts as 1
-		if (pthread_create(&data.philo[i - 1].thread, NULL, &test_routine, &data.philo[i - 1])) //returns 0 in success
-			return(ft_error("pthread_create failed", data.philo, data.num_philos, true), 3);
-	// fo the death watch thread
-	if (pthread_create(&death, NULL, &ft_death_patrol, data.philo))
-		return(ft_error("pthread_create failed", data.philo, data.num_philos, true), 3);
-	pthread_join(death, NULL);
-	// join everything and destroy the mutexes:
-	if (!ft_error(NULL, data.philo, data.num_philos, true))
+		if (pthread_create(&data.philo[i - 1].thread, NULL, &philo_routine, &data.philo[i - 1])) //** i'm passing the data.phio as the 4th argument because the philos "don't communicate with each other"
+			return(ft_exit("pthread_create failed", data.philo, data.num_philos, true), 3);
+	//? and then the death patrol
+	if (pthread_create(&check_dead, NULL, &ft_death_patrol, data.philo))
+		return(ft_exit("pthread_create failed", data.philo, data.num_philos, true), 4);
+	//? and then i join the death thread
+	pthread_join(check_dead, NULL);
+	//? and the philo threads: + destroy the mutexes
+	if (!ft_exit(NULL, data.philo, data.num_philos, true))
 		return(printf(ERROR "pthread_join failed\n"), 4);
 	// //join the threads.
 	// destroy all the mutexes for the forks
 	
 	return (EXIT_SUCCESS);
 }
-
-/* the big function i had initially and i splitted */
-
-// bool ft_init_sdata(int argc, char **argv, t_data *data)
-// {
-// 	data->num_philos = ft_atoui(argv[0]);
-// 	data->time_to_die = ft_atoui(argv[1]);
-// 	data->time_eating = ft_atoui(argv[2]);
-// 	data->time_sleeping = ft_atoui(argv[3]);
-// 	data->max_meals = 0;
-// 	if (argc == 6)
-// 		data->max_meals = ft_atoui(argv[4]);
-// 	data->any_dead = false;
-// 	// Should I also save the multiplication max_meals * num_of_philos ?
-// 	data->philo = (t_philo *)malloc(data->num_philos * sizeof(t_philo));
-// 	if (data->philo == NULL)
-// 		return(false);
-// 	// while (++id <= data->num_philos) //i starts as 1
-// 	// {
-// 	// 	data->philo[id - 1].id = id;
-// 	// 	// data->philo[id - 1].thread = 0; //do I actually need that?
-// 	// 	data->philo[id - 1].started_eat = current_mtime();
-// 	// 	data->philo[id - 1].times_ate = 0;
-// 	// 	data->philo[id - 1].is_eating = false;
-// 	// 	if (pthread_mutex_init(&data->philo[id - 1].r_fork, NULL) != 0) //returns 0 in success
-// 	// 		return (false);
-// 	// 	if (id > 1)
-// 	// 		data->philo[id - 1].l_fork = &data->philo[id - 2].r_fork;
-// 	// }
-// 	// data->philo[0].l_fork = &data->philo[data->num_philos - 1].r_fork;
-// 	// Any mutexes I want to initialize here?
-// 	return (true);
-// }
